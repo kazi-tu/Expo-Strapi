26b4c136354577db97530098e13552e55a4bba22
Hosea-kibet <kibethosea6@gmail.com>
2026-06-17 09:43:00 +0300

    strapi  spaces

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,16 +1,55 @@
 import type { Core } from '@strapi/strapi';
 
-const config: Core.Config.Middlewares = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-];
+const toOrigin = (value?: string) => {
+  if (!value) {
+    return null;
+  }
+
+  try {
+    return new URL(value).origin;
+  } catch {
+    return null;
+  }
+};
+
+const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Middlewares => {
+  const mediaOrigins = [
+    'market-assets.strapi.io',
+    toOrigin(env('SPACES_CDN_BASE_URL')),
+    toOrigin(env('SPACES_ENDPOINT')),
+    env('SPACES_BUCKET') && env('SPACES_REGION')
+      ? `https://${env('SPACES_BUCKET')}.${env('SPACES_REGION')}.digitaloceanspaces.com`
+      : null,
+    env('SPACES_BUCKET') && env('SPACES_REGION')
+      ? `https://${env('SPACES_BUCKET')}.${env('SPACES_REGION')}.cdn.digitaloceanspaces.com`
+      : null,
+  ].filter((value): value is string => Boolean(value));
+
+  return [
+    'strapi::logger',
+    'strapi::errors',
+    {
+      name: 'strapi::security',
+      config: {
+        contentSecurityPolicy: {
+          useDefaults: true,
+          directives: {
+            'connect-src': ["'self'", 'https:'],
+            'img-src': ["'self'", 'data:', 'blob:', ...mediaOrigins],
+            'media-src': ["'self'", 'data:', 'blob:', ...mediaOrigins],
+            upgradeInsecureRequests: null,
+          },
+        },
+      },
+    },
+    'strapi::cors',
+    'strapi::poweredBy',
+    'strapi::query',
+    'strapi::body',
+    'strapi::session',
+    'strapi::favicon',
+    'strapi::public',
+  ];
+};
 
 export default config;
--- config/plugins.ts
+++ config/plugins.ts
@@ -1,5 +1,32 @@
 import type { Core } from '@strapi/strapi';
 
-const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({});
+const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        baseUrl: env('SPACES_CDN_BASE_URL'),
+        rootPath: env('SPACES_ROOT_PATH', 'EXPO'),
+        s3Options: {
+          credentials: {
+            accessKeyId: env('SPACES_KEY'),
+            secretAccessKey: env('SPACES_SECRET'),
+          },
+          endpoint: env('SPACES_ENDPOINT'),
+          region: env('SPACES_REGION'),
+          params: {
+            ACL: env('SPACES_ACL', 'public-read'),
+            Bucket: env('SPACES_BUCKET'),
+          },
+        },
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+});
 
 export default config;
